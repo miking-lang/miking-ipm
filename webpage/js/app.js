d1ac653b1578d1b26262aa5cfda63c2efdf0715b
andnil5 <andnil5@kth.se>
2020-07-03 16:17:47 +0200

    Clean up before PR

--- webpage/js/app.js
+++ webpage/js/app.js
@@ -1,13 +1,13 @@
-// Renders a given DFA to the given graphviz object.
-const render = dfa => {
-    // Defining the callback function
+// Renders a given DFA at the given graphviz object.
+const render = (dfa, graph) => {
+    // Defining the callback function, which is called when the graph is rendered.
     const interactive = () =>
         // Adds on click event listeners to each of the nodes.
         d3.selectAll('.node').on("click", function () {
             let id = d3.select(this).attr('id')
             console.log(id)
         });
-    // Renders the dfa with the interactive callback function.
+        
     graph.renderDot(dfa.toDot())
         .on("end", interactive)
     }
@@ -17,5 +17,5 @@ const graph = d3.select("#graph").graphviz({zoom: false})
 
 // Checks for an existing DFA object defined in the generated source file.
 dfa
- ? render(dfa)
+ ? render(dfa, graph)
  : console.error("Error: DFA is not defined! See README.md for how use this package.")
