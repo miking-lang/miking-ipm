352f06cb413d346bd95eda0db6d327a1479fa15c
andnil5 <andnil5@kth.se>
2020-07-03 15:43:25 +0200

    Initial commit for DFA visualization

---
+++ webpage/js/data-source.js
@@ -0,0 +1,14 @@
+/**
+* Initializes the DFA object. 
+* This file was automatically generated from src/visual/boot.js.
+*/let dfa = new DFA(
+rankDirection = "LR",
+nodeSettings = { "style": "filled", "fillcolor": "white", "shape": "circle" },
+nodes = [
+    {name: "A", id:"n1", settings: {fillcolor: "green"}}
+], 
+transistions = [
+    {from: "A", to: "B", label: "0"},
+    {from: "B", to: "B", label: "0"}
+]
+)
